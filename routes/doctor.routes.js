b052f4d1df69840cf8f97d8268a40477772f208d
MahadMasood <mahadmasood85@gmail.com>
2026-01-14 07:13:53 +0500

    jwt fixes

--- routes/doctor.routes.js
+++ routes/doctor.routes.js
@@ -1,48 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Doctor = require('../models/Doctor');
-
+const Doctor = require("../models/Doctor");
+const { protect } = require("../middleware/authMiddleware");
 // @route   POST /api/doctors
 // @desc    Add a new doctor
-router.post('/', protect, adminOnly, async (req, res) => {
+router.post("/", protect, async (req, res) => {
   const { name, specialization, availableSlots } = req.body;
 
   try {
-   
-    const cleanName = name.replace(/^Dr\.\s+/i, '').replace(/\s+/g, '.').toLowerCase();
+    const cleanName = name
+      .replace(/^Dr\.\s+/i, "")
+      .replace(/\s+/g, ".")
+      .toLowerCase();
     const email = `${cleanName}@hospital.com`;
-    const defaultPassword = '123'; 
+    const defaultPassword = "123";
 
     const userExists = await User.findOne({ email });
     if (userExists) {
-      return res.status(400).json({ message: `User ${email} already exists. Cannot create duplicate.` });
+      return res
+        .status(400)
+        .json({
+          message: `User ${email} already exists. Cannot create duplicate.`,
+        });
     }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(defaultPassword, salt);
 
     const newUser = await User.create({
-      name, 
-      email, 
+      name,
+      email,
       password: hashedPassword,
-      role: 'doctor'
+      role: "doctor",
     });
 
     const newDoctor = await Doctor.create({
       _id: newUser._id,
       name,
       specialization,
-      availableSlots
+      availableSlots,
     });
 
     res.status(201).json({
       message: "Doctor and User account created successfully",
       doctor: newDoctor,
-      credentials: { email, password: defaultPassword } 
+      credentials: { email, password: defaultPassword },
     });
-
   } catch (error) {
-   
     console.error(error);
     res.status(400).json({ message: error.message });
   }
@@ -50,7 +54,7 @@ router.post('/', protect, adminOnly, async (req, res) => {
 
 // @route   GET /api/doctors
 // @desc    Get all doctors
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const doctors = await Doctor.find({});
     res.json(doctors);
